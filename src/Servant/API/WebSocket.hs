c81b6815771c3fe00c07f4b30848461828d52945
Ondrej Palkovsky <palkovsky.ondrej@gmail.com>
2018-01-05 22:20:03 +0100

    Added compatibility with servant-0.12

--- src/Servant/API/WebSocket.hs
+++ src/Servant/API/WebSocket.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP                   #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies          #-}
@@ -34,6 +35,10 @@ data WebSocket
 instance HasServer WebSocket ctx where
 
   type ServerT WebSocket m = Connection -> m ()
+
+  #if MIN_VERSION_servant_server(0,12,0)
+  hoistServerWithContext _ _ nat svr = nat . svr
+  #endif
 
   route Proxy _ app = leafRouter $ \env request respond -> runResourceT $
     runDelayed app env request >>= liftIO . go request respond
@@ -73,6 +78,10 @@ data WebSocketPending
 instance HasServer WebSocketPending ctx where
 
   type ServerT WebSocketPending m = PendingConnection -> m ()
+
+  #if MIN_VERSION_servant_server(0,12,0)
+  hoistServerWithContext _ _ nat svr = nat . svr
+  #endif
 
   route Proxy _ app = leafRouter $ \env request respond -> runResourceT $
     runDelayed app env request >>= liftIO . go request respond
--- src/Servant/API/WebSocketConduit.hs
+++ src/Servant/API/WebSocketConduit.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP                   #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
@@ -56,6 +57,10 @@ data WebSocketConduit i o
 instance (FromJSON i, ToJSON o) => HasServer (WebSocketConduit i o) ctx where
 
   type ServerT (WebSocketConduit i o) m = Conduit i (ResourceT IO) o
+
+  #if MIN_VERSION_servant_server(0,12,0)
+  hoistServerWithContext _ _ _ svr = svr
+  #endif
 
   route Proxy _ app = leafRouter $ \env request respond -> runResourceT $
     runDelayed app env request >>= liftIO . go request respond
