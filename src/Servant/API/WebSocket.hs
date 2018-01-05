c7e0eb56be3a450bfdb30cea6675047cd18e84ce
Ondrej Palkovsky <palkovsky.ondrej@gmail.com>
2018-01-05 19:51:54 +0100

    Added support for PendingConnection.

--- src/Servant/API/WebSocket.hs
+++ src/Servant/API/WebSocket.hs
@@ -9,7 +9,7 @@ import Control.Monad.IO.Class                     (liftIO)
 import Control.Monad.Trans.Resource               (runResourceT)
 import Data.Proxy                                 (Proxy (..))
 import Network.Wai.Handler.WebSockets             (websocketsOr)
-import Network.WebSockets                         (Connection, acceptRequest, defaultConnectionOptions)
+import Network.WebSockets                         (Connection, PendingConnection, acceptRequest, defaultConnectionOptions)
 import Servant.Server                             (HasServer (..), ServantErr (..), ServerT, runHandler)
 import Servant.Server.Internal.Router             (leafRouter)
 import Servant.Server.Internal.RoutingApplication (RouteResult (..), runDelayed)
@@ -44,6 +44,45 @@ instance HasServer WebSocket ctx where
     go _ respond (FailFatal e) = respond $ FailFatal e
 
     runApp a = acceptRequest >=> \c -> void (runHandler $ a c)
+
+    backupApp respond _ _ = respond $ Fail ServantErr { errHTTPCode = 426
+                                                      , errReasonPhrase = "Upgrade Required"
+                                                      , errBody = mempty
+                                                      , errHeaders = mempty
+                                                      }
+
+
+-- | Endpoint for defining a route to provide a web socket. The
+-- handler function gets a 'PendingConnection'. It can either
+-- 'rejectConnection' or 'acceptRequest'. This function is provided
+-- for greater flexibility to reject connections.
+--
+-- Example:
+--
+-- > type WebSocketApi = "stream" :> WebSocket
+-- >
+-- > server :: Server WebSocketApi
+-- > server = streamData
+-- >  where
+-- >   streamData :: MonadIO m => Connection -> m ()
+-- >   streamData c = liftIO . forM_ [1..] $ \i -> do
+-- >     forkPingThread c 10
+-- >     sendTextData c (pack $ show (i :: Int)) >> threadDelay 1000000
+data WebSocketPending
+
+instance HasServer WebSocketPending ctx where
+
+  type ServerT WebSocketPending m = PendingConnection -> m ()
+
+  route Proxy _ app = leafRouter $ \env request respond -> runResourceT $
+    runDelayed app env request >>= liftIO . go request respond
+   where
+    go request respond (Route app') =
+      websocketsOr defaultConnectionOptions (runApp app') (backupApp respond) request (respond . Route)
+    go _ respond (Fail e) = respond $ Fail e
+    go _ respond (FailFatal e) = respond $ FailFatal e
+
+    runApp a c = void (runHandler $ a c)
 
     backupApp respond _ _ = respond $ Fail ServantErr { errHTTPCode = 426
                                                       , errReasonPhrase = "Upgrade Required"
